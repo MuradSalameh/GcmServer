662546617d817c32a23aedadfbbe2c2f37f09737
Taikunon <96242268+Taikunon@users.noreply.github.com>
2022-08-18 12:35:46 +0200

    update

--- GcmServer/src/main/java/hibernate/dao/EventDAO.java
+++ GcmServer/src/main/java/hibernate/dao/EventDAO.java
@@ -2,113 +2,106 @@ package main.java.hibernate.dao;
 
 import java.util.ArrayList;
 import java.util.List;
-import main.java.hibernate.model.Event;
-import main.java.hibernate.model.Game;
-import main.java.hibernate.model.Member;
-import main.java.hibernate.model.MemberEvents;
-import main.java.hibernate.model.MemberGames;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
-
+import main.java.hibernate.model.Event;
+import main.java.hibernate.model.Member;
+import main.java.hibernate.model.MemberEvents;
 import main.java.hibernate.utils.SessionUtil;
 
 public class EventDAO {
 
-	public static void addEvent(Event bean){
-		Session session = SessionUtil.getSession();    
-		Transaction tx = session.beginTransaction();		
-		 
-		session.persist(bean);    // Dafür die add event nicht mehr aufrufen, da direkt im bean gespeichert wird.
+	public static void addEvent(Event bean) {
+		Session session = SessionUtil.getSession();
+		Transaction tx = session.beginTransaction();
+
+		session.persist(bean); // Dafür die add event nicht mehr aufrufen, da direkt im bean gespeichert wird.
 		tx.commit();
 		session.close();
 	}
-	
-	public static Event getEvent(int id) {
-		   Session session = SessionUtil.getSession();
-			Transaction tx = session.beginTransaction();
-		
-			Event event = session.get(Event.class, id);
-			
-			return event;
-	}
-	
-	public static List<Event> getEventsByMemberId(int id){		
-		//SQL: SELECT * FROM gcm.member_events where member_id= '3'
 
-		Session session = SessionUtil.getSession(); 			
-		String hql = "from MemberEvents event_id where member_id= :id";		
+	public static Event getEvent(int id) {
+		Session session = SessionUtil.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Event event = session.get(Event.class, id);
+
+		return event;
+	}
+
+	public static List<Event> getEventsByMemberId(int id) {
+		// SQL: SELECT * FROM gcm.member_events where member_id= '3'
+
+		Session session = SessionUtil.getSession();
+		String hql = "from MemberEvents event_id where member_id= :id";
 		Query query = session.createQuery(hql);
-        query.setParameter("id", id);
-		List<MemberEvents> eventsMember =  query.list();	
-		List<Event> filteredEventsList =  new ArrayList<>();	
-		
-		for(MemberEvents m : eventsMember) {
+		query.setParameter("id", id);
+		List<MemberEvents> eventsMember = query.list();
+		List<Event> filteredEventsList = new ArrayList<>();
+
+		for (MemberEvents m : eventsMember) {
 			int sId = m.getEvent().getId();
 			Event s = session.get(Event.class, sId);
 			filteredEventsList.add(s);
 			System.out.println(s);
 		}
-						
-		session.close();		
-		return filteredEventsList;		
-	}
-	
-	
-	public static List<Member> getMembersByEventId(int id){		
-		//SQL: SELECT * FROM gcm.member_events where member_id= '3'
 
-		Session session = SessionUtil.getSession(); 			
-		String hql = "from MemberEvents member_id where event_id= :id";		
+		session.close();
+		return filteredEventsList;
+	}
+
+	public static List<Member> getMembersByEventId(int id) {
+		// SQL: SELECT * FROM gcm.member_events where member_id= '3'
+
+		Session session = SessionUtil.getSession();
+		String hql = "from MemberEvents member_id where event_id= :id";
 		Query query = session.createQuery(hql);
-        query.setParameter("id", id);
-		List<MemberEvents> membersEvent =  query.list();	
-		List<Member> filteredEventsList =  new ArrayList<>();	
-		
-		for(MemberEvents m : membersEvent) {
+		query.setParameter("id", id);
+		List<MemberEvents> membersEvent = query.list();
+		List<Member> filteredEventsList = new ArrayList<>();
+
+		for (MemberEvents m : membersEvent) {
 			int sId = m.getMember().getId();
 			Member s = session.get(Member.class, sId);
 			filteredEventsList.add(s);
 			System.out.println(s);
 		}
-						
-		session.close();		
-		return filteredEventsList;		
+
+		session.close();
+		return filteredEventsList;
 	}
 
-
-
-	public static List<Event> getEvents(){
-		Session session = SessionUtil.getSession();  
+	public static List<Event> getEvents() {
+		Session session = SessionUtil.getSession();
 		String hql = "from Event";
 		Query<Event> query = session.createQuery(hql);
-		List<Event> events =  query.list();		
-		session.close();		
+		List<Event> events = query.list();
+		session.close();
 		return events;
 	}
-	
-	
-	public static void deleteEventFromMember(int id){		
-		Session session = SessionUtil.getSession(); 
+
+	public static void deleteEventFromMember(int id) {
+		Session session = SessionUtil.getSession();
 		Transaction tx = session.beginTransaction();
-		
+
 		// Delete connection from MemberEvents Table
-		String hql = "delete from MemberEvents id where event_id= :id";		
-		Query query = session.createQuery(hql);		
-        query.setParameter("id", id);
-        
-        int count = query.executeUpdate();
-        System.out.println(count + " Record(s) Deleted.");
-        
-        // Remove from Event Table
+		String hql = "delete from MemberEvents id where event_id= :id";
+		Query query = session.createQuery(hql);
+		query.setParameter("id", id);
+
+		int count = query.executeUpdate();
+		System.out.println(count + " Record(s) Deleted.");
+
+		// Remove from Event Table
 //    	Event event = session.get(Event.class, id);
 //		session.remove(event);
 
-        tx.commit();
-        session.clear();
-        session.close();
+		tx.commit();
+		session.clear();
+		session.close();
 	}
 
 	public static void deleteEvent(int id) {
@@ -118,28 +111,27 @@ public class EventDAO {
 		session.remove(event);
 		tx.commit();
 		session.close();
-		
+
 	}
 
-	public static void updateEvent(int id, Event event){
+	public static void updateEvent(int id, Event event) {
 		Session session = SessionUtil.getSession();
 		Transaction tx = session.beginTransaction();
 		Event old = session.get(Event.class, id);
-		
+
 		old.setEventTitle(event.getEventTitle());
 		old.setEventDescription(event.getEventDescription());
 		old.setDate(event.getDate());
-		old.setEventStartTime(event.getEventEndTime());
+		old.setEventStartTime(event.getEventStartTime());
 		old.setEventEndTime(event.getEventEndTime());
 		old.setEventAddidtionalNotes(event.getEventAddidtionalNotes());
 		old.setReoccuring(event.isReoccuring());
-		//old.setMembers(event.getMembers());		
-		
+		// old.setMembers(event.getMembers());
+
 		session.saveOrUpdate(old);
 		session.flush();
 		tx.commit();
-		session.close();		
+		session.close();
 	}
-
 
 }
